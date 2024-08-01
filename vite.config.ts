140714278650c18d027746ec1a54f076bdcd603f
Ildar <ildar.1795@gmail.com>
2024-08-01 20:42:44 +0400

    feat: Добавляет pwa в проект

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      display: 'standalone',
+      name: 'pwa-notes-app',
+      short_name: 'n.a.',
+      description: 'It is pwa notes app',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'favicon'
+        },
+        {
+          src: '/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'favicon'
+        },
+        {
+          src: '/icons/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+          purpose: 'apple touch icon',
+        },
+        {
+          src: '/icons/icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+          purpose: 'any maskable'
+        },
+      ]
+    },
+  })],
 })
